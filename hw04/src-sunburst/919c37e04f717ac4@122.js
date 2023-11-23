174c5ba88ca9e49c5c10713954e1ea30b13ed93e
Pokai su <56548670+winston98321@users.noreply.github.com>
2023-11-24 03:33:52 +0800

    Update 919c37e04f717ac4@122.js

--- hw04/src-sunburst/919c37e04f717ac4@122.js
+++ hw04/src-sunburst/919c37e04f717ac4@122.js
@@ -444,7 +444,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["artist.csv", {url: new URL("./atrist.csv", import.meta.url), mimeType: "text/csv", toString}]
+    ["artist.csv", {url: new URL("./artist.csv", import.meta.url), mimeType: "text/csv", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
